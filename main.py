4a287f20c00593c2e3524fc7002f669adb21edd8
jonathanlys01 <jonathanlystahiti@gmail.com>
2023-05-17 08:59:01 +0000

    added classes to main.py

--- main.py
+++ main.py
@@ -24,202 +24,228 @@ import random
 from sklearn.preprocessing import StandardScaler
 
 ######################################################"
-import argparse
-parser = argparse.ArgumentParser(description='Provide path to the parser')
 
-parser.add_argument('--path',"-p", default="current/input/video_standing_good.mp4", help='Path to the desired file')
-args = parser.parse_args()
-path_to_process = args.path
+
 
 print("you are in ",os.getcwd())
 
 # setting up the config
-cfg = get_cfg()  
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9 
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml")
 
+class Detector:
+    def __init__(self):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml"))
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
+        self.thr = self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml")
+        self.predictor = DefaultPredictor(self.cfg)
+        self.last_file = ""
+        self.last_output = ""
+        self.ann_model = NeuralNetwork(57,[128,64],1)
+        self.ann_model.load_state_dict(torch.load("best_model.pt")["model_state_dict"])
+        print("Setup Done")
 
-predictor = DefaultPredictor(cfg)
-
-def process(input_path):
-    # input_path : path to video
+    def process_image(self, im):
+        # returns the processed image (can be shown with cv2.imshow())
+        outputs = self.predictor(im)
+        if len(outputs["instances"]) and all(list(i>self.thr for i in (outputs["instances"]).scores)):
+            v = Visualizer(im[:,:,::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1)
+            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        return out.get_image()[:, :, ::-1]
     
-    cap = cv2.VideoCapture(input_path)
-    global predictor
+    def process_video(self,input_path):
+        # input_path : path to video
+        cap = cv2.VideoCapture(input_path)
 
-    boxes = []
-    segments = []
-    keypoints = []
+        boxes = []
+        segments = []
+        keypoints = []
 
-    if (cap.isOpened()== False): 
-        print("Error opening video stream or file")
-        return
+        if (cap.isOpened()== False): 
+            print("Error opening video stream or file")
+            return
 
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret == True:
-            if not(frame is None):
-                metadata = {'w': frame.shape[1],'h': frame.shape[0]}
-            # do something on frame
-            outputs = predictor(frame)['instances'].to('cpu')
-            has_bbox = False
-            if outputs.has('pred_boxes'):
-                bbox_tensor = outputs.pred_boxes.tensor.numpy()
-                if len(bbox_tensor) > 0:
-                    has_bbox = True
-                    scores = outputs.scores.numpy()[:, None]
-                    bbox_tensor = np.concatenate((bbox_tensor, scores), axis=1)
-            if has_bbox:
-                kps = outputs.pred_keypoints.numpy()
-                kps_xy = kps[:, :, :2]
-                kps_prob = kps[:, :, 2:3]
-                kps_logit = np.zeros_like(kps_prob) # Dummy
-                kps = np.concatenate((kps_xy, kps_logit, kps_prob), axis=2)
-                kps = kps.transpose(0, 2, 1)
-            else:
-                kps = []
-                bbox_tensor = []
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                if not(frame is None):
+                    metadata = {'w': frame.shape[1],'h': frame.shape[0]}
+                # do something on frame
+                outputs = self.predictor(frame)['instances'].to('cpu')
+                has_bbox = False
+                if outputs.has('pred_boxes'):
+                    bbox_tensor = outputs.pred_boxes.tensor.numpy()
+                    if len(bbox_tensor) > 0:
+                        has_bbox = True
+                        scores = outputs.scores.numpy()[:, None]
+                        bbox_tensor = np.concatenate((bbox_tensor, scores), axis=1)
+                if has_bbox:
+                    kps = outputs.pred_keypoints.numpy()
+                    kps_xy = kps[:, :, :2]
+                    kps_prob = kps[:, :, 2:3]
+                    kps_logit = np.zeros_like(kps_prob) # Dummy
+                    kps = np.concatenate((kps_xy, kps_logit, kps_prob), axis=2)
+                    kps = kps.transpose(0, 2, 1)
+                else:
+                    kps = []
+                    bbox_tensor = []
 
-            # Mimic Detectron1 format
-            cls_boxes = [[], bbox_tensor]
-            cls_keyps = [[], kps]
+                # Mimic Detectron1 format
+                cls_boxes = [[], bbox_tensor]
+                cls_keyps = [[], kps]
 
-            boxes.append(cls_boxes)
-            segments.append(None)
-            keypoints.append(cls_keyps)
-        else: 
-            break
+                boxes.append(cls_boxes)
+                segments.append(None)
+                keypoints.append(cls_keyps)
+            else: 
+                break
 
-    cap.release()
+        cap.release()
 
-    # Closes all the frames
-    cv2.destroyAllWindows()
-    
-    name = "processed"+str(uuid.uuid4())
-    
-    #metadata = np.array(metadata,dtype=object)
-    """np.savez_compressed(file=f"current/middle/{name}.npz",keypoints=keypoints,
-                        metadata=metadata,boxes=boxes,segments=segments)"""
-    
-    # var :keypoints,boxes, metadata
-    print("Processed video with detectron2")
-    
-    bb = boxes
-    kp = keypoints
-
-    results_bb = []
-    results_kp = []
-    for i in range(len(bb)):
-        if len(bb[i][1]) == 0 or len(kp[i][1]) == 0:
-            # No bbox/keypoints detected for this frame -> will be interpolated
-            results_bb.append(np.full(4, np.nan, dtype=np.float32)) # 4 bounding box coordinates
-            results_kp.append(np.full((17, 4), np.nan, dtype=np.float32)) # 17 COCO keypoints
-            continue
-        best_match = np.argmax(bb[i][1][:, 4])
-        best_bb = bb[i][1][best_match, :4]
-        best_kp = kp[i][1][best_match].T.copy()
-        results_bb.append(best_bb)
-        results_kp.append(best_kp)
+        # Closes all the frames
+        cv2.destroyAllWindows()
         
-    bb = np.array(results_bb, dtype=np.float32)
-    kp = np.array(results_kp, dtype=np.float32)
-    kp = kp[:, :, :2] # Extract (x, y)
+        
+        # generates a random unique name 
+        name = "processed"+str(uuid.uuid4())
     
-    # Fix missing bboxes/keypoints by linear interpolation
-    mask = ~np.isnan(bb[:, 0])
-    indices = np.arange(len(bb))
-    for i in range(4):
-        bb[:, i] = np.interp(indices, indices[mask], bb[mask, i])
-    for i in range(17):
-        for j in range(2):
-            kp[:, i, j] = np.interp(indices, indices[mask], kp[mask, i, j])
-    
-    print('{} total frames processed'.format(len(bb)))
-    print('{} frames were interpolated'.format(np.sum(~mask)))
-    
-    print('----------')
-   
-    
-    coco_metadata = {
-    'layout_name': 'coco',
-    'num_joints': 17,
-    'keypoints_symmetry': [
-        [1, 3, 5, 7, 9, 11, 13, 15],
-        [2, 4, 6, 8, 10, 12, 14, 16],
-                        ]
-        }
-  
-    
-    
-    
-    output = {
-        name:{
-        "custom":[kp.astype("float32")]
-               }
-               }
+        # var :keypoints,boxes, metadata
+        print("Processed video with detectron2")
+        
+        bb = boxes
+        kp = keypoints
 
+        results_bb = []
+        results_kp = []
+        for i in range(len(bb)):
+            if len(bb[i][1]) == 0 or len(kp[i][1]) == 0:
+                # No bbox/keypoints detected for this frame -> will be interpolated
+                results_bb.append(np.full(4, np.nan, dtype=np.float32)) # 4 bounding box coordinates
+                results_kp.append(np.full((17, 4), np.nan, dtype=np.float32)) # 17 COCO keypoints
+                continue
+            best_match = np.argmax(bb[i][1][:, 4])
+            best_bb = bb[i][1][best_match, :4]
+            best_kp = kp[i][1][best_match].T.copy()
+            results_bb.append(best_bb)
+            results_kp.append(best_kp)
+            
+        bb = np.array(results_bb, dtype=np.float32)
+        kp = np.array(results_kp, dtype=np.float32)
+        kp = kp[:, :, :2] # Extract (x, y)
+        
+        # Fix missing bboxes/keypoints by linear interpolation
+        mask = ~np.isnan(bb[:, 0])
+        indices = np.arange(len(bb))
+        for i in range(4):
+            bb[:, i] = np.interp(indices, indices[mask], bb[mask, i])
+        for i in range(17):
+            for j in range(2):
+                kp[:, i, j] = np.interp(indices, indices[mask], kp[mask, i, j])
+        
+        print('{} total frames processed'.format(len(bb)))
+        print('{} frames were interpolated'.format(np.sum(~mask)))
+        
+        print('----------')
     
-    coco_metadata['video_metadata'] = {name:metadata}
-    
-    print('Saving...')
-    
-    output_prefix_2d = 'data_2d_custom_'
-    
-    np.savez_compressed(f"VideoPose3D/data/{output_prefix_2d+name}.npz", 
-    positions_2d=output, metadata=coco_metadata)
-    
-    #np.savez_compressed(f"current/output/{name}.npz", positions_2d=output, metadata=metadata)
-    
-    print('Done.')
-    
-    print("Dataset ready for inference 3d")
-    
-    # return the name of the input file and the associated file
-    file_name = os.path.basename(input_path)
-    
-    return {"input":file_name,
-            "output":name,
+        
+        coco_metadata = {
+        'layout_name': 'coco',
+        'num_joints': 17,
+        'keypoints_symmetry': [
+            [1, 3, 5, 7, 9, 11, 13, 15],
+            [2, 4, 6, 8, 10, 12, 14, 16],
+                            ]
             }
     
+        output = {
+            name:{
+            "custom":[kp.astype("float32")]
+                }
+                }
+
+        
+        coco_metadata['video_metadata'] = {name:metadata}
+        
+        print('Saving...')
+        
+        output_prefix_2d = 'data_2d_custom_'
+        
+        # can not be saved somewhere else.
+        np.savez_compressed(f"VideoPose3D/data/{output_prefix_2d+name}.npz", 
+        positions_2d=output, metadata=coco_metadata)
+        
+        print('Done.')
+        
+        print("Dataset ready for inference 3d")
+        
+        # return the name of the input file and the associated file
+        file_name = os.path.basename(input_path) # without the extension
+        
+        self.last_file = file_name
+        self.last_output = name
+        
+        return {"input":file_name,
+                "output":name,
+                }
+        
+    def run_pose3d(self):
+        dict_dataset = {"input":self.last_file,
+                        "output":self.last_output}
+        PATH_TO_ELT = f"../current/input/{dict_dataset['input']}"
+        PATH_FINAL_VID = f"../current/output/vis_{dict_dataset['output']}.mp4"
+        PATH_FINAL_ARR = f"../current/output/arr_{dict_dataset['output']}.npy"
+
+        args =[
+        "-d", "custom",
+        "-k", dict_dataset["output"],
+        "-arc", "3,3,3,3,3",
+        "-c", "checkpoint",
+        "--evaluate", "pretrained_h36m_detectron_coco.bin",
+        "--render", 
+        "--viz-subject", dict_dataset["output"],
+        "--viz-action", "custom",
+        "--viz-camera", "0",
+        "--viz-video", PATH_TO_ELT,
+        "--viz-export", PATH_FINAL_ARR,
+        "--viz-output", PATH_FINAL_VID,
+        "--viz-size", "6"
+        ]
+
+        command = ["python", "run.py"] + args
+
+        subprocess.run(command, check=True,cwd='VideoPose3D/')
     
+    def apply_ann(self):
+        # returns the mean of the 5 predictions
+        results_3D = np.load(f"current/output/arr_{self.last_output}.npy")
+        assert results_3D.shape[0]>21
+        X_processed = feature_creation(results_3D)
+        shift = list(range(-10,10))
+        random.shuffle(shift)
+        selected_shift = shift[:5]
+        n = X_processed.shape[0]
+        L_results = []
+        for elt_shift in selected_shift:
+            x_tensor = torch.from_numpy(X_processed[n//2+elt_shift])
+            L_results.append(float(self.ann_model(x_tensor)))
+        return np.mean(L_results)
+        
 
-    
-# change path here
-dict_dataset = process(path_to_process)
-
-print(dict_dataset)
-
-print("Infering 3D")
-
-
-PATH_TO_ELT = f"../current/input/{dict_dataset['input']}"
-PATH_FINAL_VID = f"../current/output/vis_{dict_dataset['output']}.mp4"
-PATH_FINAL_ARR = f"../current/output/arr_{dict_dataset['output']}.npy"
-
-args =[
-  "-d", "custom",
-  "-k", dict_dataset["output"],
-  "-arc", "3,3,3,3,3",
-  "-c", "checkpoint",
-  "--evaluate", "pretrained_h36m_detectron_coco.bin",
-  "--render", 
-  "--viz-subject", dict_dataset["output"],
-  "--viz-action", "custom",
-  "--viz-camera", "0",
-  "--viz-video", PATH_TO_ELT,
-  "--viz-export", PATH_FINAL_ARR,
-  "--viz-output", PATH_FINAL_VID,
-  "--viz-size", "6"
-]
-
-command = ["python", "run.py"] + args
-
-subprocess.run(command, check=True,cwd='VideoPose3D/')
-
-results_3D = np.load(f"current/output/arr_{dict_dataset['output']}.npy")
-assert results_3D.shape[0]>21
-
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(NeuralNetwork, self).__init__()
+        self.layer_1 = nn.Linear(input_dim, hidden_dim[0])
+        self.relu = nn.ReLU()
+        self.layer_2 = nn.Linear(hidden_dim[0], hidden_dim[1])
+        self.layer_3 = nn.Linear(hidden_dim[1], output_dim)
+        self.sigmoid = nn.Sigmoid()
+       
+    def forward(self, x):
+        x = self.relu(self.layer_1(x))
+        x = self.relu(self.layer_2(x))
+        x = self.sigmoid(self.layer_3(x))
+        return x
+   
 
 def feature_creation(array):
 
@@ -281,42 +307,9 @@ def feature_creation(array):
     return X
 
     
-
-X_processed = feature_creation(results_3D)
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(NeuralNetwork, self).__init__()
-        self.layer_1 = nn.Linear(input_dim, hidden_dim[0])
-        self.relu = nn.ReLU()
-        self.layer_2 = nn.Linear(hidden_dim[0], hidden_dim[1])
-        self.layer_3 = nn.Linear(hidden_dim[1], output_dim)
-        self.sigmoid = nn.Sigmoid()
-       
-    def forward(self, x):
-        x = self.relu(self.layer_1(x))
-        x = self.relu(self.layer_2(x))
-        x = self.sigmoid(self.layer_3(x))
-        return x
-       
-input_dim = X_processed.shape[1]
-hidden_dim = [128, 64]  # Number of neurons in each hidden layer
-output_dim = 1
-
-model = NeuralNetwork(input_dim, hidden_dim, output_dim)
-
-
-state_dict = torch.load("best_model.pt")["model_state_dict"]
-model.load_state_dict(state_dict)
-
-
-shift = list(range(-10,10))
-random.shuffle(shift)
-
-selected_shift = shift[:5]
-n = X_processed.shape[0]
-
-for elt_shift in selected_shift:
-    x_tensor = torch.from_numpy(X_processed[n//2+elt_shift])
-    print(float(model(x_tensor)))
+if __name__ == "__main__":
+    detector = Detector()
+    path_to_process = "current/input/video_standing_good.mp4"
+    print(detector.process_video(path_to_process))
+    detector.run_pose3d()
+    print(detector.apply_ann())
